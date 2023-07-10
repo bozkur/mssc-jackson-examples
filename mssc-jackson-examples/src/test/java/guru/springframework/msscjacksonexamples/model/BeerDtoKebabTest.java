f286db5210ea9232d309683917d03b2dc1db0089
cevher.bozkur <bozkur@gmail.com>
2023-07-10 23:59:35 +0300

    Solution for Assignment#1.
    Using kebab strategy in property naming strategy.

--- mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
+++ mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoKebabTest.java
@@ -5,9 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("snake")
+@ActiveProfiles("kebab")
 @JsonTest
-class BeerDtoSnakeTest extends BaseTest {
+class BeerDtoKebabTest extends BaseTest {
 
     @Test
     void shouldSerializeDto() throws JsonProcessingException {
@@ -19,7 +19,7 @@ class BeerDtoSnakeTest extends BaseTest {
 
     @Test
     void shouldDeserializeDto() throws JsonProcessingException {
-        String json = "{\"created_date\":\"2023-07-10T23:48:28.1611186+03:00\",\"last_modified_date\":\"2023-07-10T23:48:28.1611186+03:00\",\"id\":\"552c42e4-b15b-4107-b7a3-d43d4a16fb0d\",\"beer_name\":\"Efes Dark\",\"beer_style\":\"Lager\",\"upc\":123,\"price\":9.99}";
+        String json = "{\"created-date\":\"2023-07-10T23:48:28.1611186+03:00\",\"last-modified-date\":\"2023-07-10T23:48:28.1611186+03:00\",\"id\":\"552c42e4-b15b-4107-b7a3-d43d4a16fb0d\",\"beer-name\":\"Efes Dark\",\"beer-style\":\"Lager\",\"upc\":123,\"price\":9.99}";
 
         BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
         System.out.println(beerDto);
--- mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
+++ mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoTest.java
@@ -1,16 +1,12 @@
 package guru.springframework.msscjacksonexamples.model;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
 
 @JsonTest
 class BeerDtoTest extends BaseTest {
 
-    @Autowired
-    private ObjectMapper objectMapper;
     @Test
     void shouldSerializeDto() throws JsonProcessingException {
         BeerDto beerDto = getDto();
