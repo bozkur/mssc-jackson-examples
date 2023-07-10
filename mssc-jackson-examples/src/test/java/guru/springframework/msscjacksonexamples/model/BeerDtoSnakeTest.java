004e3451cf9ca1c81a9768e63f5fbf1a18604ea7
cevher.bozkur <bozkur@gmail.com>
2023-07-10 23:50:21 +0300

    Property naming strategy SNAKE_CASE yapılarak, serialize ve deserialize işlemleri gerçekleştirildi.

--- mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
+++ mssc-jackson-examples/src/test/java/guru/springframework/msscjacksonexamples/model/BeerDtoSnakeTest.java
@@ -5,12 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("snake")
 @JsonTest
-class BeerDtoTest extends BaseTest {
+class BeerDtoSnakeTest extends BaseTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
     @Test
     void shouldSerializeDto() throws JsonProcessingException {
         BeerDto beerDto = getDto();
@@ -18,10 +21,11 @@ class BeerDtoTest extends BaseTest {
 
         System.out.println(jsonString);
     }
-    
+
     @Test
     void shouldDeserializeDto() throws JsonProcessingException {
-        String json = "{\"createdDate\":\"2023-07-10T23:36:08.4551524+03:00\",\"lastModifiedDate\":\"2023-07-10T23:36:08.4551524+03:00\",\"id\":\"e9e701d6-8776-4912-9c68-0e54585b13b9\",\"beerName\":\"Efes Dark\",\"beerStyle\":\"Lager\",\"upc\":123,\"price\":9.99}";
+        String json = "{\"created_date\":\"2023-07-10T23:48:28.1611186+03:00\",\"last_modified_date\":\"2023-07-10T23:48:28.1611186+03:00\",\"id\":\"552c42e4-b15b-4107-b7a3-d43d4a16fb0d\",\"beer_name\":\"Efes Dark\",\"beer_style\":\"Lager\",\"upc\":123,\"price\":9.99}";
+
         BeerDto beerDto = objectMapper.readValue(json, BeerDto.class);
         System.out.println(beerDto);
     }
